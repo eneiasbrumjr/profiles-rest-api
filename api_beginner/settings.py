cd5651eda8466ea13553d4ea4ee21f343a008679
Eneias S Brum Jr <eneiassbrumjr@gmail.com>
2023-09-22 12:15:10 -0300

    Added custom user profile model, manager and migrations

--- api_beginner/settings.py
+++ api_beginner/settings.py
@@ -119,6 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+AUTH_USER_MODEL = 'project_beginner_api.UserProfile'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
